5c38e7a1098c4739ce984d140bd6e26a5bbe6bd0
roboactive2324 <vasmo02@gmail.com>
2025-02-14 11:45:25 +0530

    part two (design less)- daman

--- SISHackathon/hackathon/src/main/java/com/javahackers/FinanceManager.java
+++ SISHackathon/hackathon/src/main/java/com/javahackers/FinanceManager.java
@@ -3,6 +3,8 @@ package com.javahackers;
 import java.util.Scanner;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -15,10 +17,10 @@ public class FinanceManager {
         frame.setVisible(true);
         frame.setLayout(null);
         JTextField deposit = new JTextField();
-        deposit.setBounds(500,265, 500,300);
+        deposit.setBounds(500,225, 500,300);
         frame.add(deposit);
         JLabel stuffie = new JLabel();
-        stuffie.setBounds(0, 0, 100, 10);
+        stuffie.setBounds(0, 0, 1, 1);
         frame.add(stuffie);
         deposit.addActionListener(new ActionListener() {
             @Override
@@ -27,5 +29,41 @@ public class FinanceManager {
                 stuffie.setText(stuffie.getText()+"\n"+temp);
             }
         });
+        JButton button = new JButton();
+        button.setBounds(10, 225, 490,300);
+        button.setText("Click to add record of Transaction/Deposit");
+        frame.add(button);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = deposit.getText();
+                stuffie.setText(stuffie.getText()+"\n"+temp + ", ");
+            }
+        });
+        JButton button2 = new JButton();
+        button2.setBounds(1000, 225, 490,150);
+        button2.setText("Click to see History of Transactions/Deposits");
+        frame.add(button2);
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = deposit.getText();
+                stuffie.setBounds(500, 0, 500, 1000);
+                deposit.setBounds(0,0, 1,1);
+            };
+        });
+        JButton button3 = new JButton();
+        button3.setBounds(1000, 375, 490,150);
+        button3.setText("Click to add record of Transaction/Deposit again");
+        frame.add(button3);
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = deposit.getText();
+                stuffie.setBounds(0, 0, 1, 1);
+                deposit.setBounds(500,225, 500,300);
+            };
+        });
+
     }
 }
